b8f54000f682e1bb1cb695639a08fb108b5b8ea7
Ludovic Scyboz <scyboz@mpp.mpg.de>
2017-04-03 11:55:17 +0200

    New version with 8 TeV as well

--- Work/27.02.17_MCPROD_TUNING_RZG/linScan.py
+++ Work/27.02.17_MCPROD_TUNING_RZG/linScan.py
@@ -11,11 +11,57 @@ def findYODA(i,j,k,index):
   for d in SubDirPath("."):
     if d.find(str(i)+"_"+str(j)+"_"+str(k))!=-1:
 
+     if d.find("7000")!=-1:
 	print d
 	os.chdir(d)
 	print "Copying YODA file "+d.split("/")[1]
 	os.system("cp *177.yoda ../scan/mc/"+str(index))
-	os.chdir("..")
+
+	os.chdir("../scan/mc/"+str(index))
+	for f in os.listdir("."):
+	  if f.find(".yoda")!=-1 and f.find("7000")!=-1:
+	    name = f	
+
+	    lines = open(name,'r').readlines()
+	    newYODA = open(str(index)+"_7TeV.yoda",'w')
+	    flag = True
+	    for line in lines:
+	      if line.find("ATLAS_2013_")!=-1:
+	        flag = False
+	      if line.find("ATLAS_2014_")!=-1:
+	        flag = True
+	      if flag == True:
+	        newYODA.writelines(line)
+	    os.system("rm "+name)
+#	    os.system("yoda2aida "+str(index)+".yoda "+str(index)+".aida")
+
+
+	os.chdir("../../../")
+     if d.find("8000")!=-1:
+        print d
+        os.chdir(d)
+        print "Copying YODA file "+d.split("/")[1]
+        os.system("cp *253.yoda ../scan/mc/"+str(index))
+
+	os.chdir("../scan/mc/"+str(index))
+        for f in os.listdir("."):
+          if f.find(".yoda")!=-1 and f.find("8000")!=-1:
+            name = f
+
+            lines = open(name,'r').readlines()
+            newYODA = open(str(index)+"_8TeV.yoda",'w')
+            flag = True
+            for line in lines:
+              if line.find("Parton")!=-1:
+                flag = False
+              if line.find("custom")!=-1:
+                flag = True
+              if flag == True:
+                newYODA.writelines(line)
+            os.system("rm "+name)
+
+
+        os.chdir("../../../")
   os.chdir("scan")
 	
 
@@ -23,7 +69,7 @@ alphaSMZ = [0.125, 0.140]
 ClMaxLight = [0.5, 3.5]
 PSplitLight = [0.5, 3.5]
 
-nsteps = 2 
+nsteps = 4 
 nstepsClMaxLight = 3
 nstepsPSplit = 3
 
